8b4143d3778064d0738c20a82cbf517be725418d
calderon104 <gonza.rc1004@gmail.com>
2021-12-08 11:04:33 -0300

    QUE ANDE POR DIOS

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Espero q funcione esta vez");
+var user= new User();
+user.Name = "Gonzalo";
+user.Surname = "Calderon";
+
+
+Console.WriteLine(user.Fullname());
+
+
+var yo = new Yo();
+yo.Ciudad= "San Luis";
+yo.Pais= "Argentina";
+
+Console.WriteLine(yo.DondeVive());
